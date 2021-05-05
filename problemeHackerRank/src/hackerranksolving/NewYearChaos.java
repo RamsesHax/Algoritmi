9ea9cb11b05ed95358a8e459c03dfa892908a532
RamsesHax <onetiu.rares@yahoo.com>
2021-05-05 11:03:31 +0300

    Second way for this algo

--- problemeHackerRank/src/hackerranksolving/NewYearChaos.java
+++ problemeHackerRank/src/hackerranksolving/NewYearChaos.java
@@ -42,4 +42,35 @@ public class NewYearChaos {
 		}
 	    }
 	
+	public static void minimumBribesV2(int[] q) {
+		//This version is with nested loops but the performance leaks on larger test cases.
+		int bribes = 0 ; 	int flag  = 0 ;
+		
+		for(int i = 0 ; i < q.length ; i++) {
+			int test = 0;
+			for(int j = i ; j<q.length ; j++) {
+				if(q[i]>q[j]) {
+					test++;
+				}
+			}
+			
+			if(test >2) {
+				System.out.println("Too chaotic");
+				flag =1;
+				break;
+			}
+			else if(test == 2) {
+				bribes+=2;
+				test = 0;
+			}
+			else if(test ==1) {
+				bribes++;
+				test = 0;
+			}
+		}
+		if(flag == 0) {
+			System.out.println(bribes);
+		}
+	}
+	
 }
